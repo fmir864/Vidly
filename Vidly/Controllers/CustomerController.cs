f5b6382fc2ff8366218485c64067dcccea31d56d
FM Irshad <fmir864@outlook.com>
2018-12-22 21:11:33 +0530

    Added Model and Controller code for Movie and Customer and set layout links

---
+++ Vidly/Controllers/CustomerController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+
+            return View(customers);
+        }
+
+        private static List<Customer> GetCustomers()
+        {
+            return new List<Customer>()
+            {
+                new Customer { Id = 1, Name = "FM Irshad" },
+                new Customer { Id = 2, Name = "Mosh Hamedani" }
+            };
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id.Equals(id));
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+    }
+}
---
+++ Vidly/Controllers/MovieController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Index()
+        {
+            var movies = new List<Movie>()
+            {
+                new Movie { Name = "Venom" },
+                new Movie { Name = "The Predator" }
+            };
+
+            return View(movies);
+        }
+    }
+}
