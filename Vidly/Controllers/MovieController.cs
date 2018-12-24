57d6b9180be52728d9ec025ee96432eb92edca5d
FM Irshad <fmir864@outlook.com>
2018-12-24 22:08:21 +0530

    Movie list and details page

--- Vidly/Controllers/MovieController.cs
+++ Vidly/Controllers/MovieController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,16 +10,33 @@ namespace Vidly.Controllers
 {
     public class MovieController : Controller
     {
+        private ApplicationDbContext _context = null;
+
+        public MovieController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Movie
         public ActionResult Index()
         {
-            var movies = new List<Movie>()
-            {
-                new Movie { Name = "Venom" },
-                new Movie { Name = "The Predator" }
-            };
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
 
             return View(movies);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id.Equals(id));
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
         }
     }
 }
